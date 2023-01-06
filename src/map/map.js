02a605f059a41dc8f8c9351894b826408d3b8fba
backSword <thibault.quartierditmaire@orange.fr>
2023-01-06 12:53:25 +0100

    basic map implementation

--- src/map/map.js
+++ src/map/map.js
@@ -1,6 +1,14 @@
 
 
-class TileMap
+class Map
 {
-    Tile[][]
-}
+
+    /** 
+     * @param {string} map representing tile 
+     * @param bytes of  
+    */
+    constructor(map,tileMap){
+        this.map = map;
+        this.tileMap = tileMap;
+    }
+} 
--- src/map/tile.js
+++ src/map/tile.js
@@ -1,4 +1,21 @@
 class Tile
 {
-    
+    /**  @type string */
+    sprite
+
+    /**
+     * @param {*} phaser 
+     * @param {string} sprite 
+     * @param {string} spriteUrl 
+     */
+    constructor (phaser, sprite, spriteUrl) 
+    {
+        this.sprite = sprite;
+        phaser.load.image(sprite, spriteUrl);
+    }
+
+    draw(phaser)
+    {
+        phaser.add.image(this.sprite);
+    }
 }
