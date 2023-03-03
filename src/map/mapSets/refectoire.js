d2ba2974d3cfe220094835e2e0cafcd9f0fe7cbb
invertime <invertime@gmail.com>
2023-03-03 12:50:33 +0100

    added maps

---
+++ src/map/mapSets/refectoire.js
@@ -0,0 +1,19 @@
+let tilebackground = [
+    [1,1,1,1,1],
+    [1,1,1,1,1],
+    [1,1,1,1,1],
+    [1,1,1,1,1],
+    [1,1,1,1,1]
+];
+
+let tileFront =  [
+    [1,1,1,1,1],
+    [1,1,1,1,1],
+    [1,1,1,1,1],
+    [1,1,1,1,1],
+    [1,1,1,1,1]
+];
+
+let tiles = [tilebackground, tileFront];
+
+export default tiles;
